5b943bab4ac8f3fe359856de807e6825f6cc2fb5
e-sawyer <109112191+ewsawyer@users.noreply.github.com>
2023-02-09 13:08:06 -0800

    Add lesson 0 materials

---
+++ Lesson 0/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Message from './Message.jsx'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+    </div>
+  );
+}
+
+export default App;
---
+++ Lesson 0/src/Message.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Message() {
+    return <h1>what's up this is my message</h1>
+}
+
+export default Message
